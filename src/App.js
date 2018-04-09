cfca9b26c04f7037148be6423250838eb0e2d801
Marto <martinkaldramov1@gmail.com>
2018-04-09 18:24:30 +0300

    Added the Material UI library to the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // @flow
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 
 // class App extends Component {
 //   render() {
@@ -21,15 +24,18 @@ import './App.css';
 // }
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  <MuiThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+    <RaisedButton>Material UI</RaisedButton>
+  </MuiThemeProvider>
 );
 
 const add = (a, b) => a + b;
