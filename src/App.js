4723c6d4d20e0c6e32e63db4c916615fc31b000d
Marto <martinkaldramov1@gmail.com>
2018-04-14 09:40:35 +0300

    Refactored the App.js component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 // @flow
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import AppBarExampleIconButton from './containers/app_bar'
 
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+import AppBarExampleIconButton from './containers/app_bar';
 
 const App = () => (
   <MuiThemeProvider>
