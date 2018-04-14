24e5653fe2ddb1706e455dc0e1505e71f34c07fc
Marto <martinkaldramov1@gmail.com>
2018-04-14 12:52:29 +0300

    Added a test case for the App component whch is currently failing

---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+describe('App shoud render 2 components', () => {
+  const wrap = shallow(App);
+  expect(wrap).toMatchSnapshot();
+});
