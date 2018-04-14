47cf0afc5bf05e105a8384b67687de0b3b7ad304
Marto <martinkaldramov1@gmail.com>
2018-04-14 13:05:58 +0300

    Wrote 2 tests for the App component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 
-describe('App shoud render 2 components', () => {
+describe('App Component tests', () => {
   const wrap = shallow(<App />);
-  expect(wrap).toMatchSnapshot();
+
+  it('Should render 2 child components', () => {
+    expect(wrap.children().length).toBe(2);
+  });
+
+  it('should render successfully', () => {
+    expect(wrap).toMatchSnapshot();
+  });
 });
