3ff1a5b79dc99e6804aa1fb225123329615476e6
Marto <martinkaldramov1@gmail.com>
2018-04-14 14:59:26 +0300

    Created FilterBar Component (errors with thests and Flow)

--- src/containers/search_results/filter_bar.js
+++ src/containers/search_results/filter_bar.js
@@ -1,23 +1,45 @@
 // @flow
-import React from 'react';
+import * as React from 'react';
+// Material UI imports
+import FlatButton from 'material-ui/FlatButton';
+
 import ActiveFilters from './active_filters';
 import StaticFilters from './static_filters';
 import InputFilters from './input_filters';
-
-// Material UI imports
-import FlatButton from 'material-ui/FlatButton';
 
 const mockCofig = {
   jobsFound: 286542,
 };
 
-let filters:  = []
+type filtersTypes = {
+  name: string,
+  categorie: string,
+  active: boolean,
+};
+
+const filters: Array<filtersTypes> = [
+  {
+    name: 'Any Type',
+    categorie: 'Types',
+    active: true,
+  },
+  {
+    name: 'Intemediate',
+    categorie: 'Experience',
+    active: true,
+  },
+  {
+    name: 'Any Type',
+    categorie: 'Types',
+    active: true,
+  },
+];
 
 const FilterBar = () => (
   <React.Fragment>
     <p>Showing `${mockCofig.jobsFound}` results found</p>
     <FlatButton label="Filters" />
-    <ActiveFilters />
+    <ActiveFilters filters={filters} />
     <StaticFilters />
     <InputFilters />
   </React.Fragment>
