75897762f05438bedefb678f40b9a7f74577a813
Marto <martinkaldramov1@gmail.com>
2018-04-15 08:33:58 +0300

    Refactored Job

--- src/components/search_results/job.js
+++ src/components/search_results/job.js
@@ -1,11 +1,14 @@
 // @flow
 import * as React from 'react';
-import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 
 type Props = {
   title: string,
   views: number,
-  date: {},
+  company: string,
+  // eslint-disable-next-line
+  date: ?{},
   city: string,
   type: string,
   experience: string,
@@ -14,6 +17,28 @@ type Props = {
   skills: string[],
 };
 
-const Job = (props: Props) => <p>{props.title}</p>;
+const Job = (props: Props) => (
+  <Card>
+    <CardHeader
+      title={props.title}
+      subtitle={`
+        ${props.company} 
+        ${props.views}
+        ${props.city} 
+        ${props.type} 
+        ${props.experience} 
+        ${props.category}
+      `}
+    />
+    <CardText>
+      {props.description}
+      {props.skills}
+    </CardText>
+    <CardActions>
+      <RaisedButton label="Like" />
+      <RaisedButton label="Save" />
+    </CardActions>
+  </Card>
+);
 
 export default Job;
