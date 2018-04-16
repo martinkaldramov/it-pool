14e0023de1d8708719fc0382ba5cce8e3b3ea87b
Marto <martinkaldramov1@gmail.com>
2018-04-16 06:09:20 +0300

    Fixed type in SearchBar test

--- src/tests/search_results/search_results.test.js
+++ src/tests/search_results/search_results.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import SearchResults from '../../containers/search_results/search_results';
 
 describe('SearchResults', () => {
-  const wrap = shalow(<SearchResults />);
+  const wrap = shallow(<SearchResults />);
 
   it('should render successfully', () => {
     expect(wrap).toMatchSnapshot();
