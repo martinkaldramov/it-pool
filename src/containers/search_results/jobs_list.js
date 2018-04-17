2ffe608e8d256e38eeb18a04daf190d727bf4f14
Marto <martinkaldramov1@gmail.com>
2018-04-17 06:54:37 +0300

    JobList now iterates over array of jobs

--- src/containers/search_results/jobs_list.js
+++ src/containers/search_results/jobs_list.js
@@ -70,23 +70,9 @@ const jobs: job[] = [
   },
 ];
 
-const job1 = {
-  title: 'Proofreading/light editing of a legal document translated from Japanese to English',
-  views: 236,
-  company: 'MODIS BULGARIA',
-  date: new Date().getDate(),
-  city: 'Sofia',
-  type: 'Full Time',
-  experience: 'Solid',
-  category: 'Web Development',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet facilisis ligula, ac consectetur nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce venenatis condimentum tristique. Quisque volutpat vulputate dolor, id rhoncus erat euismod id. Sed maximus dignissim arcu ac pretium. Curabitur vel ante at nunc sodales consectetur. Proin et porta sem, ac volutpat tellus. Curabitur ipsum lacus, gravida id fermentum id, pellentesque vitae erat.',
-  skills: ['React.JS', 'Angular.io', 'DevOps', 'ExpressJS', 'Google Firebase'],
-};
-
 const JobsList = () => (
   <React.Fragment>
-    <Job jobData={job1} />
+    <ul>{jobs.map(item => <Job jobData={item} />)}</ul>
   </React.Fragment>
 );
 
