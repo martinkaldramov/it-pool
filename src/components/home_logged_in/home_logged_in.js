53972de30e249bd17807981dd566ed660e8b78d2
Marto <martinkaldramov1@gmail.com>
2018-04-18 06:11:02 +0300

    Created HomeLoggedIn

---
+++ src/components/home_logged_in/home_logged_in.js
@@ -0,0 +1,13 @@
+// @flow
+import * as React from 'react';
+import SideMenu from './side_menu';
+import JobsList from '../reusable/jobs_list';
+
+const HomeLoggedIn = (): React.Node => (
+  <React.Fragment>
+    <SideMenu />
+    <JobsList />
+  </React.Fragment>
+);
+
+export default HomeLoggedIn;
