c7efa13547b2e2a392afe7c79cd1e30a6a18e918
Marto <martinkaldramov1@gmail.com>
2018-04-18 07:24:22 +0300

    Extracted static menu items in seperate component - StaticMenuItems

--- src/components/reusable/side_menu.js
+++ src/components/reusable/side_menu.js
@@ -5,12 +5,8 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
 import ActionHome from 'material-ui/svg-icons/action/home';
-import NotificationEventNote from 'material-ui/svg-icons/notification/event-note';
-import ActionEvent from 'material-ui/svg-icons/action/event';
-import ContentSave from 'material-ui/svg-icons/content/save';
-import ActionSearch from 'material-ui/svg-icons/action/search';
-import SocialWhatshot from 'material-ui/svg-icons/social/whatshot';
-import ActionExplore from 'material-ui/svg-icons/action/explore';
+
+import StaticMenuItems from '../reusable/static_menu_items';
 
 const SideMenu = (): React.Node => (
   <Drawer docked>
@@ -18,24 +14,7 @@ const SideMenu = (): React.Node => (
       <ActionHome />Home
     </MenuItem>
     <Divider />
-    <MenuItem>
-      <NotificationEventNote />My Applications
-    </MenuItem>
-    <MenuItem>
-      <ActionEvent />My Proposals
-    </MenuItem>
-    <MenuItem>
-      <ContentSave />Saved Jobs
-    </MenuItem>
-    <MenuItem>
-      <ActionSearch />Saved Searches
-    </MenuItem>
-    <MenuItem>
-      <SocialWhatshot />Popular Jobs
-    </MenuItem>
-    <MenuItem>
-      <ActionExplore />Browse Categories
-    </MenuItem>
+    <StaticMenuItems />
   </Drawer>
 );
 
---
+++ src/components/reusable/static_menu_items.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import MenuItem from 'material-ui/MenuItem';
+
+import NotificationEventNote from 'material-ui/svg-icons/notification/event-note';
+import ActionEvent from 'material-ui/svg-icons/action/event';
+import ContentSave from 'material-ui/svg-icons/content/save';
+import ActionSearch from 'material-ui/svg-icons/action/search';
+import SocialWhatshot from 'material-ui/svg-icons/social/whatshot';
+import ActionExplore from 'material-ui/svg-icons/action/explore';
+
+const StaticMenuItems = () => (
+  <React.Fragment>
+    <MenuItem>
+      <NotificationEventNote />My Applications
+    </MenuItem>
+    <MenuItem>
+      <ActionEvent />My Proposals
+    </MenuItem>
+    <MenuItem>
+      <ContentSave />Saved Jobs
+    </MenuItem>
+    <MenuItem>
+      <ActionSearch />Saved Searches
+    </MenuItem>
+    <MenuItem>
+      <SocialWhatshot />Popular Jobs
+    </MenuItem>
+    <MenuItem>
+      <ActionExplore />Browse Categories
+    </MenuItem>
+  </React.Fragment>
+);
+
+export default StaticMenuItems;
