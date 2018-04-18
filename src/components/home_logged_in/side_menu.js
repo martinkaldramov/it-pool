afd99eda2ebe864dbdaafd6376993c4cfc16f180
Marto <martinkaldramov1@gmail.com>
2018-04-18 07:13:21 +0300

    Added all static menu items

--- src/components/home_logged_in/side_menu.js
+++ src/components/home_logged_in/side_menu.js
@@ -7,6 +7,10 @@ import MenuItem from 'material-ui/MenuItem';
 import ActionHome from 'material-ui/svg-icons/action/home';
 import NotificationEventNote from 'material-ui/svg-icons/notification/event-note';
 import ActionEvent from 'material-ui/svg-icons/action/event';
+import ContentSave from 'material-ui/svg-icons/content/save';
+import ActionSearch from 'material-ui/svg-icons/action/search';
+import SocialWhatshot from 'material-ui/svg-icons/social/whatshot';
+import ActionExplore from 'material-ui/svg-icons/action/explore';
 
 const SideMenu = (): React.Node => (
   <Drawer docked>
@@ -19,6 +23,18 @@ const SideMenu = (): React.Node => (
     </MenuItem>
     <MenuItem>
       <ActionEvent />My Proposals
+    </MenuItem>
+    <MenuItem>
+      <ContentSave />Saved Jobs
+    </MenuItem>
+    <MenuItem>
+      <ActionSearch />Saved Searches
+    </MenuItem>
+    <MenuItem>
+      <SocialWhatshot />Popular Jobs
+    </MenuItem>
+    <MenuItem>
+      <ActionExplore />Browse Categories
     </MenuItem>
   </Drawer>
 );
