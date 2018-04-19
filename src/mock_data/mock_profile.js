cfd86574c11d5ffa1f798f074c5a9a130e64f472
Marto <martinkaldramov1@gmail.com>
2018-04-19 05:31:57 +0300

    Added recentSearches and Categories proeprties to mock profile

--- src/mock_data/mock_profile.js
+++ src/mock_data/mock_profile.js
@@ -1,7 +1,29 @@
 // @flow
 
-type MockProfileType = {};
+type MockProfileType = {
+  recentSearches: string[],
+  categories: string[],
+};
 
-const MockProfile: MockProfileType = {};
+const MockProfile: MockProfileType = {
+  recentSearches: [
+    'Web Developer',
+    'linux system administrator',
+    'nodejs backend developer',
+    'ReactJS Developer',
+    'network engineer',
+    'devops',
+    'C# desktop developer',
+  ],
+  categories: [
+    'Web Development',
+    'Web & Mobile Design',
+    'Scripts & Utilities',
+    'Desktop Software Development',
+    'Quality Assurance',
+    'Frontend Web Development',
+    'Network Engineering',
+  ],
+};
 
 export default MockProfile;
