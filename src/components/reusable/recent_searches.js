f46ea7c9b1370a844e2d4a227df2f090f9d641e5
Marto <martinkaldramov1@gmail.com>
2018-04-20 04:32:01 +0300

    Refactored searchTerms to return results from MockProfile

--- src/components/reusable/recent_searches.js
+++ src/components/reusable/recent_searches.js
@@ -1,18 +1,15 @@
 // @flow
 import * as React from 'react';
+import MenuItem from 'material-ui/MenuItem';
 import MockProfile from '../../mock_data/mock_profile';
 
-// const RecentSearches = () => <ul>{lastFive.map(item => <li>{item}</li>)}</ul>;
+const searchTerms = MockProfile.recentSearches.map(term => <li>{term}</li>);
 
 const RecentSearches = () => (
-  <ul>
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-    <li>Item 4</li>
-    <li>Item 5</li>
-    <li>Item 6</li>
-  </ul>
+  <React.Fragment>
+    <MenuItem>RECENT SEARCHES</MenuItem>
+    <ul>{searchTerms}</ul>
+  </React.Fragment>
 );
 
 export default RecentSearches;
