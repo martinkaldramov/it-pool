81420134ffb3b9f4538017f612fce74db9943871
Marto <martinkaldramov1@gmail.com>
2018-04-20 06:07:06 +0300

    Fixed double display of AppBar

--- src/components/home_logged_in/home_logged_in.js
+++ src/components/home_logged_in/home_logged_in.js
@@ -1,11 +1,9 @@
 // @flow
 import * as React from 'react';
-import SideMenu from '../reusable/side_menu';
 import JobsList from '../reusable/jobs_list';
 
 const HomeLoggedIn = (): React.Node => (
   <React.Fragment>
-    <SideMenu />
     <JobsList />
   </React.Fragment>
 );
