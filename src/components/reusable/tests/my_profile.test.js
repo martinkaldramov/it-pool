3673d232b89767593b1f9dac44429aecce48b06b
Marto <martinkaldramov1@gmail.com>
2018-04-20 05:01:52 +0300

    MyProfile successful render test

---
+++ src/components/reusable/tests/my_profile.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MyProfile from '../my_profile';
+
+describe('MyProfile' () => {
+  const wrap = shallow(<MyProfile />);
+
+  it('should render successfully', () => {
+    expect(wrap).toMatchSnapshot();
+  });
+});
